0d26c1ece0b33c5dfaebdc610c0a25682aa0b2d1
Glitch (balsam-clumsy-dosa) <none>
2024-01-31 20:39:32 +0000

    👨💴 Checkpoint ./server.js:34363899/1712 ./package.json:34363899/522

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // server.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 const commonData = require('./common.js');
 
@@ -30,7 +31,61 @@ app.get('/admin', (req, res) => {
   res.render('admin');
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+  }
+);
+//------------------------------------------------------------
+
+var messages = [];
+
+function doTrainerCommand(data) {
+  if (data.body == "delete") {
+    messages = [];
+    return;
+  }
+  if (data.body == "clear") {
+    for (let i = 0; i < messages.length; i++) {
+      messages[i].body = "";
+    }
+    return;
+  }
+  if (data.body.startsWith("deletename")) {
+    const studentName = data.body.replace("deletename ","").toLowerCase();
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i].name.toLowerCase() == studentName) {
+        messages.splice(i, 1);
+        break;
+      }
+    }
+  }
+}
+
+function saveMessage(data) {
+  for (let i = 0; i < messages.length; i++) {
+    if (messages[i].name.toLowerCase() == data.name.toLowerCase()) {
+      messages[i].body = data.body;
+      return;
+    }
+  }
+
+  messages.push({ name: data.name, body: data.body });
+}
+
+io.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    if (data.name.toLowerCase() == "trainer") {
+      doTrainerCommand(data);
+    } else {
+      saveMessage(data);
+    }
+
+    io.sockets.emit("message", messages);
+  });
 });
